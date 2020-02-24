894a8fdd685bb0a3cba13de0d8e1d6da123ffaa7
Haydon Houghton <haydonhoughton@outlook.com>
2020-02-24 17:35:35 +0000

    Add listing page and confirm listing page

---
+++ spec/features/create_listing_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+feature 'create a listing' do
+  scenario 'user enters a name, description and price for their space' do
+    visit('/listings/create')
+    fill_in "name_field", with: "Cute ass cottage"
+    fill_in "desc_field", with: "This cottage is deadass so cute"
+    fill_in "price_field", with: "69.69"
+    click_button("Add Listing")
+    expect(page).to have_content "Your space has been listed!"
+  end
+end
