4ba53d472e9ba0e268249a49fdf342715461f695
Josh Blumberg <jlblumberg@gmail.com>
2020-02-25 09:13:14 +0000

    Add webhelper to dry up feature tests

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,22 +1,18 @@
+require_relative '../webhelpers.rb'
+
 feature 'User Log-In' do
 
-  scenario 'A non-existant user cannot log in' do
-    visit('/')
-    fill_in('email', with: 'josh@wemail.com')
-    fill_in('password', with: 'secretpassword')
-    click_button("Sign up")
+  scenario 'A non-existing user cannot log in' do
+    sign_up # as josh@wemail.com
     expect(page).to have_content("Please log in")
-    fill_in('email', with: 'pete@wemail.com')
+    fill_in('email', with: 'pete@wemail.com') # won't match, different email
     fill_in('password', with: 'secretpassword')
     click_button("Log in")
     expect(page).to have_content("Please log in")
   end
 
   scenario 'An existing user can log in' do
-    visit('/')
-    fill_in('email', with: 'josh@wemail.com')
-    fill_in('password', with: 'secretpassword')
-    click_button("Sign up")
+    sign_up # as josh@wemail.com
     expect(page).to have_content("Please log in")
     fill_in('email', with: 'josh@wemail.com')
     fill_in('password', with: 'secretpassword')
