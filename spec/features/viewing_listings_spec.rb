b9f67f891b611de37ee19e349329510aff1de996
Haydon Houghton <haydonhoughton@outlook.com>
2020-02-25 12:08:15 +0000

    Add webhelper for sign up and sign in

--- spec/features/viewing_listings_spec.rb
+++ spec/features/viewing_listings_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 feature 'view listing page' do
   scenario 'user wants to see their listing after creation' do
+    sign_up_and_sign_in
     create_cottage_listing
     expect(page).to have_content('Cute ass cottage')
   end
--- spec/webhelpers.rb
+++ spec/webhelpers.rb
@@ -5,18 +5,19 @@ def sign_up
   click_button("Sign up")
 end
 
-def create_cottage_listing
-  visit('/listings/create')
-  fill_in "name_field", with: "Cute ass cottage"
-  fill_in "desc_field", with: "This cottage is deadass so cute"
-  fill_in "price_field", with: "69"
-  click_button("Add Listing")
-end
-
 def sign_up_and_sign_in
   sign_up # as josh@wemail.com
   expect(page).to have_content("Please log in")
   fill_in('email', with: 'josh@wemail.com')
   fill_in('password', with: 'secretpassword')
   click_button("Log in")
+end
+
+def create_cottage_listing
+  sign_up_and_sign_in
+  visit('/listings/create')
+  fill_in "name_field", with: "Cute ass cottage"
+  fill_in "desc_field", with: "This cottage is deadass so cute"
+  fill_in "price_field", with: "69"
+  click_button("Add Listing")
 end
