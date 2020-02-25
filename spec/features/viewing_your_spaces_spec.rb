f29a05b3879a0cb48511b9ee9322b8873b09d6f8
Richard Pattinson <richie.pattinson@googlemail.com>
2020-02-25 14:09:00 +0000

    fixed tests to match restful routes

--- spec/features/viewing_your_spaces_spec.rb
+++ spec/features/viewing_your_spaces_spec.rb
@@ -4,7 +4,8 @@ feature 'view your spaces' do
   scenario 'user wants to see their spaces after creation' do
     sign_up_and_sign_in
     create_cottage_listing
-    click_link("Back to spaces")    
+    click_link("Back to spaces")
+    click_link("Your Listed Spaces")   
     expect(page).to have_content('Cute ass cottage')
   end
 end
