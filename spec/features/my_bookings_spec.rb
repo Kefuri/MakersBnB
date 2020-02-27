52a589f403f8c77fab796e12660270e04abf950c
Chris Eadie <chris.eadie@outlook.com>
2020-02-27 12:24:45 +0000

    Add more robust testing

--- spec/features/my_bookings_spec.rb
+++ spec/features/my_bookings_spec.rb
@@ -1,15 +1,38 @@
 feature 'My bookings page' do
-  scenario 'visiting bookings/list shows a booking' do
-    create_users_and_space_and_booking
+  scenario 'visiting bookings/list shows a single booking after creating one booking' do
+    create_two_users_and_one_space_and_one_booking
     visit('/login')
     # josh is the user who is making this booking
     fill_in('email', with: 'josh@wemail.com')
     fill_in('password', with: 'secretpassword')
     click_button("Log in")
-    visit('/bookings/list')
+    visit('/booking/yours')
 
-    expect(page).to have_content("Bob's space") # space name
-    expect(page).to have_content("01/01/20") # booking start date
-    expect(page).to have_content("02/01/20") # booking end date
+    # space name
+    expect(page).to have_content("Bob's space")
+    # booking start date
+    expect(page).to have_content("2020-12-13")
+    # booking end date 
+    expect(page).to have_content("2020-12-14")
+  end
+
+  scenario 'visiting bookings/list shows a bookings list after creating two bookings' do
+    create_three_users_and_two_spaces_and_two_bookings
+    visit('/login')
+    # josh is the user who is making this booking
+    fill_in('email', with: 'josh@wemail.com')
+    fill_in('password', with: 'secretpassword')
+    click_button("Log in")
+    visit('/booking/yours')
+
+    # space1 details
+    expect(page).to have_content("Bob's space")
+    expect(page).to have_content("2020-12-13")
+    expect(page).to have_content("2020-12-14")
+
+    # space2 details
+    expect(page).to have_content("Ed's space")
+    expect(page).to have_content("2020-07-20")
+    expect(page).to have_content("2020-07-21")
   end
 end
